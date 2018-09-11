a9080dadafb15b99309c5b17543ba4306490d2aa
alessandroZelli <42467805+alessandroZelli@users.noreply.github.com>
2018-09-11 12:33:12 +0200

    Tester  update
    Added negative testing, now the tester also displays which services were stopped from the selected list

--- Tester.ps1
+++ Tester.ps1
@@ -28,8 +28,16 @@ Write-Host
 $oldServList=@(Import-Csv -Path $filePathMaster -Delimiter ';' )
 $oldNameList =[System.Collections.ArrayList]::new()
 foreach($item in $oldServList){
-    $itemName = $item.Name
-    $oldNameList.Add($itemName) | out-null
+	$itemName = $item.Name
+	if($itemName -match "^[a-zA-z]+[_]+[0-9a-zA-Z]+"){
+		#parses out the PID written after the "_" character, to avoid false positives
+		$pos =$itemName.IndexOf("_")
+		$leftPart = $itemName.Substring(0, $pos)
+		$oldNameList.Add($leftPart) | out-null
+	}
+	else{
+		$oldNameList.Add($itemName) | out-null
+	}
 }
 #Write-Host $oldNameList #Optional, used for debug
 
@@ -37,34 +45,79 @@ Write-Host "Service List acquired from csv"
 Write-Host
 Write-Host
 Write-Host
+#Imports the active services list and saves it in a string list containing only the services' names
+$newNameList =[System.Collections.ArrayList]::new()
 $newServList=@(Get-Service | Where-Object {$_.Status -eq 'running'})
-$outputArr=[System.Collections.ArrayList]::new()
-
-
-Write-Host  "Testing..."
-
-#Tests wether the new service names are contained in the old list, if not, saves them to the output list.
-foreach ($j in $newServList){
-$newName=$j.Name
-if (-not($oldNameList.Contains($newName)))
-{
-    $outputArr.Add($newName) | Out-Null
-       
-   }
+foreach ($item in $newServList){
+	$itemName = $item.Name
+	if($itemName -match "^[a-zA-z]+[_]+[0-9a-zA-Z]+"){
+		#parses out the PID written after the "_" character, to avoid false positives
+		$pos =$itemName.IndexOf("_")
+		$leftPart = $itemName.Substring(0, $pos)
+		$newNameList.Add($leftPart) | out-null
+	}
+	else{
+		$newNameList.Add($item.Name)| out-null
+	}
 }
 
-pause
-Write-Host
-Write-Host
-Write-Host
-if($outputArr.Count -eq 0){
-    write-host "No active service is absent from the master list"
+
+
+
+#newly activated services output list
+$outputArrAct=[System.Collections.ArrayList]::new()
+
+#stopped services output list 
+$outputArrStop=[System.Collections.ArrayList]::new()
+
+Write-Host "Testing..."
+
+#Tests wether the new service names are contained in the old list, if not, saves them to the newly activated output list.
+foreach ($j in $newNameList){
+	#$newName=$j.Name
+	if (-not($oldNameList.Contains($j))){
+		$outputArrAct.Add($j) | Out-Null
+    }
 }
-else{
+
+#Tests wether the old service names are contained in the new list, if not, saves them to the stopped services output list.
+foreach ($n in $oldNameList){
+	#$oldName=$n.Name
+	if (-not($newNameList.Contains($n))){
+		$outputArrStop.Add($n) | Out-Null
+	}
+}
+$selectedFileName= $filePathMaster.split("\")[-1]
+Write-Host
+Write-Host
+Write-Host "RESULT"
+Write-Host "Selected File: $selectedFileName;"
+Write-Host
 Write-Host "Newly activated services: "
 Write-Host
-Write-Host
-Write-Host $outputArr
-pause
+
+if($outputArrAct.Count -eq 0){
+    Write-Host "No active service is absent from the selected snapshot."
+	Write-Host
+	Write-Host
+	
 }
+else{
+	$actResult = $outputArrAct | Out-String
+	Write-Host $actResult
+	Write-Host
+	Write-Host
+}
+Write-Host "Services stopped from selected snapshot: "
+Write-Host
+if($outputArrStop.Count -eq 0){
+	Write-Host "No services were stopped from the selected snapshot."
+}
+else{
+	$stopResult = $outputArrStop | Out-String
+	Write-Host $stopResult
+	Write-Host
+	Write-Host
+}
+pause
 
